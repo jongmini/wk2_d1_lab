d1f4043a75b3608c9f01caf8995c0decae3b6057
jongmini <jongmin.choi@gmail.com>
2014-01-13 23:10:37 -0800

    couldnt figure out all of it

--- sorted_array.rb
+++ sorted_array.rb
@@ -5,14 +5,14 @@ class SortedArray
     @internal_arr = input_arr
     # Fill in the rest of the initialize method here.
     # What should you do with each element of the incoming array?
-    first_larger_index(@internal_arr[0], start_ind=1, end_ind=@internal_arr.size)
+
+    self.first_larger_index(@internal_arr[0], start_ind=1, end_ind=@internal_arr.size)
     #hmm... is this the right way?
 
   end
 
   def add(new_ele)
 
-    # @internal_arr.insert(-1,@new_ele)
     # Hint: Use the Array#insert method.
     @internal_arr.insert(
     first_larger_index(new_ele, start_ind=0, end_ind=@internal_arr.size), new_ele)
@@ -24,6 +24,10 @@ class SortedArray
   end
 
   def [](index)
+
+    #if index is a range then use index.to_a to convert to an array.
+    #but how do I check if index is a range rather than a single value?
+    #also can use index.first and index.last
 
     if index < @internal_arr.length
       @internal_arr.fetch(index)
@@ -51,13 +55,34 @@ class SortedArray
         return first_larger_index(target, mid_ind+1, end_ind)
       else target < @internal_arr[mid_ind]
         return first_larger_index(target, start_ind, mid_ind)
-
       end 
     end
 
   end
 
   def index(target)
-    raise NotImplementedError.new("You need to implement the index method!")
+    
+      find_match(target, start_ind=0, end_ind=@internal_arr.size)
+
   end
+
+  def find_match(target, start_ind=0, end_ind=@internal_arr.size)
+
+    mid_ind = (end_ind + start_ind)/2
+
+    if not @internal_arr.include?(target)
+      nil
+    else 
+      if target == @internal_arr[mid_ind] 
+        return mid_ind
+      elsif @internal_arr[0...mid_ind].include?(target)
+        return find_match(target, start_ind=0, mid_ind)
+      else @internal_arr[mid_ind+1...end_ind].include?(target)
+        return find_match(target, mid_ind+1, end_ind)
+
+      end 
+    end
+  end
+
+
 end
