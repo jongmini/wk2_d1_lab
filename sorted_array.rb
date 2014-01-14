5f4221a65772efd333192203735b556cdb3fb6de
jongmini <jongmin.choi@gmail.com>
2014-01-14 10:09:38 -0800

    fixed index with the original idea of binary

--- sorted_array.rb
+++ sorted_array.rb
@@ -1,4 +1,4 @@
-class SortedArray
+class SortedArray 
   attr_accessor :internal_arr # This is for convenience in the tests.
 
   def initialize(input_arr=[])
@@ -23,7 +23,7 @@ class SortedArray
 
   def [](index)
     
-    # previous code - didn't work
+    # previous code  didn't work
     # if index < @internal_arr.length
     #   @internal_arr.fetch(index)
     # else index >= @internal_arr.length
@@ -42,7 +42,7 @@ class SortedArray
     mid_ind = (end_ind + start_ind)/2
 
     if @internal_arr.size == 0 # terminal conditions
-      return internal_arr.size
+      return 0
     elsif target > @internal_arr[-1]
       return internal_arr.size
     elsif start_ind >= end_ind
@@ -51,7 +51,7 @@ class SortedArray
       if target == @internal_arr[mid_ind] 
         return mid_ind
       elsif target > @internal_arr[mid_ind] # if greater than, recursive with the latter half
-        return first_larger_index(target, mid_ind+1, end_ind)
+        return first_larger_index(target, mid_ind + 1, end_ind)
       else target < @internal_arr[mid_ind] # if less than, recursive with the first half
         return first_larger_index(target, start_ind, mid_ind)
       end 
@@ -59,11 +59,30 @@ class SortedArray
 
   end
 
-  def index(target)
-    
-    return @internal_arr.index(target)
 
-  end
+    # stu's code
+    # @internal_arr.includ?(target) ? first_larger_index(target) : nil
+
+    def index(target, start_ind=0, end_ind=@internal_arr.size)
+  
+     mid_ind = (end_ind + start_ind)/2
+ 
+     if start_ind >= end_ind
+       return nil
+     else 
+       if target == @internal_arr[mid_ind] 
+         return mid_ind
+       elsif target > @internal_arr[mid_ind]
+         return index(target, mid_ind+1, end_ind)
+       else 
+         return index(target, start_ind=0, mid_ind)
+ 
+       end 
+     end
+    end
+
 
 
 end
+
+
